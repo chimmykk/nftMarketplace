5e910bb7e122982c988f03e1e1cef0f8cab32f37
dhruv <dhrvmohapatra@gmail.com>
2021-08-14 20:16:41 +0530

    feat: Add user interface

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,118 @@
-import styles from '../styles/Home.module.css'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { nftAddress, nftMarketAddress } from "../config";
+//import ABIs
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    // use ether provider
+    const provider = new ethers.providers.JsonRpcProvider();
+    // grab token and market contracts
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftMarketAddress,
+      Market.abi,
+      provider
+    );
+
+    // async call to the items in the marketplace
+    const data = await marketContract.fetchMarketItems();
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenUri(i.tokenId);
+        const meta = await axios.ethers(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ethers");
+        // nft
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+
+  async function buyNFT(nft) {
+    // connect to metamask etc.
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(nftAddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ethers");
+
+    const transaction = await contract.createMarketSale(
+      nftAddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
+    // wait for the transaction to go through and then load the existing NFTs
+    await transaction.wait();
+    loadNFTs();
+  }
+
+  // when there's no items in the marketplace
+  if (loadingState === "loaded" && !nfts.length)
+    return (
+      <h1 className="px-20 py-20 text-3xl">No items in the marketplace</h1>
+    );
+
   return (
-    <div className={styles.container}>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => {
+            <div key={i} className="border shadow rounded-xl oveflow-hidden">
+              <img src={nft.image} />
+              <div className="p-4">
+                <p
+                  className="text-2xl font-semibold"
+                  style={{ height: "64px" }}
+                >
+                  {nft.name}
+                </p>
+                <div style={{ height: "70px", overflow: "hidden" }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl mb-4 font-bold text-white">
+                  {nft.price} Matic
+                </p>
+                <button
+                  className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                  onClick={() => buyNFT(nft)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>;
+          })}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
